092e0ae7483b2367d4400373f715f0d7fae223b4
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-02-04 15:20:09 -0300

    Agrego background y cambio un poco el margin

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { phrase } from './assets/phrases.js';
 import LivesBar from "./components/LivesBar.jsx";
 import './css/font.css';
 
-const urlGifImage = `/src/assets/${Math.floor(Math.random() * 7) + 1}.gif?q=${Date.now()}`;
 const lsAccept = localStorage.getItem('accept') === 'true';
 const lsLives = localStorage.getItem('lives') ? parseInt(localStorage.getItem('lives')) : 8;
 const totalLives = 8;
@@ -47,16 +46,16 @@ export default function App() {
   }
 
   return (
-    <div className='flex items-center justify-center bg-rose-200 min-h-screen '>
+    <div className='flex items-center justify-center bg-rose-200 min-h-screen bg-image'>
       <div className="flex items-center justify-center flex-col bg-rose-300 md:max-w-full md:h-screen md:p-5 rounded-lg border-dashed border-2 border-rose-600 animate-fade-up animate-once animate-ease-in-out">
         <div className="flex items-center justify-center">
           {lives === 0 ?
             <img src={`/src/assets/lose.gif?q='${Date.now()}`} alt="No San Valentin </3" />
-            : <img src={urlGifImage} alt="San Valentin <3" />}
+            : <img src={`/src/assets/${lives}.gif?q=${Date.now()}`} alt="San Valentin <3" />}
         </div>
 
         <div className="flex flex-col items-center my-3">
-          {!accept && lives != 0 && <div className="text-rose-500 text-3xl md:text-6xl text text-center yxj-font">¿Serías mi San Valentín?</div>}
+          {!accept && lives != 0 && <div className="text-rose-500 text-3xl md:text-6xl mx-2 text text-center yxj-font">¿Serías mi San Valentín?</div>}
 
           {!accept && lives == 0 && <div className="text-rose-500 text-3xl md:text-6xl text text-center yxj-font"> Okey... </div>}
 
@@ -68,12 +67,12 @@ export default function App() {
         </div>
 
         {!accept && lives != 0 && <div className="mb-3">
-          <button className="bg-rose-400 hover:bg-red-500 text-black font-bold py-2 px-4 md:py-3 md:px-6 mr-1 rounded-md yxj-font"
+          <button className="bg-rose-400 hover:bg-red-500 text-rose-100 font-bold py-2 px-4 md:py-3 md:px-6 mr-1 rounded-md yxj-font"
             onClick={yes}>o SI o</button>
         </div>}
         
         {!accept && lives != 0 && <div className="pb-3">
-          <button ref={noRef} className={`${!noIsDisabled ? 'bg-blue-300 hover:bg-blue-400' : 'bg-gray-300'} text-white font-bold py-2 px-4 md:py-3 md:px-6 rounded-md yxj-font`}
+          <button ref={noRef} className={`${!noIsDisabled ? 'bg-blue-300 hover:bg-blue-400' : 'bg-gray-300'} text-white font-bold py-2 px-4 mx-4 mb-2 md:py-3 md:px-6 rounded-md yxj-font`}
             onClick={no} disabled={noIsDisabled}>× NO ×</button>
         </div>}
 
