befaa5cabf884ace19aaa268c9904ab4f023b600
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-02-09 01:13:21 -0300

    Borrar frases que no me convencian

--- src/assets/phrases.js
+++ src/assets/phrases.js
@@ -5,14 +5,9 @@ export const phrase = [
     "¿Suuuuuuuuuuuuuuper segura que no?",
     "¿Un momento para pensarlo mejor? Te lo desactivo unos segunditos, pensarlo bien esta vez",
     "¿Y si hay chispa? Pika! Pika! ⚡",
-    "¿Segura de que quieres perder esta oportunidad?",
     "¿No hay ninguna posibilidad? En la lotería con 0.0000001% ya se gana, digo nomas",
     "¿Ni una mariposita en el estomago?",
-    "¿No ni un poquito hay curiosidad en el si? 'La curiosidad mato al gato' es solo un dicho",
-    "¿Segura de que no hay magia?",
-    "¿Ni una poquito de interés?",
-    "¿No hay un sí oculto en ese corazoncito?",
+    "¿No ni un poquito hay curiosidad en el si? La curiosidad mato al gato pero a la gata",
     "¿Estás segura de rechazar esta oferta limitada? Es una promoción especial solo por hoy",
-    "Si no le das al 'Si', te vas a quedar con las ganas de saber qué habría pasado",
 ]
 
