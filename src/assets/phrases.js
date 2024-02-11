6639788911bed466177252520d3e72d3a7a3ad01
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-02-10 22:57:31 -0300

    Fix de ortográfico

--- src/assets/phrases.js
+++ src/assets/phrases.js
@@ -1,7 +1,7 @@
 export const phrases = [
     { text: "¿Segura que no ibas a decir si? Pista: El 'Si' es el botón de arriba", ms: 3000 },
-    { text: "Todavía te noto con al duda, deberías pensarlo un poquito más, ¿no?", ms: 3000 },
+    { text: "Todavía te noto con la duda, deberías pensarlo un poquito más, ¿no?", ms: 3000 },
     { text: "¿Estás segura de rechazar esta oferta limitada? Es una promoción especial solo por hoy", ms: 3000 },
-    { text: "¿Un momento para pensarlo mejor? Te lo desactivo unos segunditos, piensalo bien esta vez :)", ms: 8000 },
+    { text: "¿Un momento para pensarlo mejor? Te lo desactivo unos segunditos, piénsalo bien esta vez :)", ms: 8000 },
 ];
 
