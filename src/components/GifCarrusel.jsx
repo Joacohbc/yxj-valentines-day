7a081351956dcc3aa96e18631bfd9a4180d6f1e2
Joacohbc <joaquingenovag8@gmail.com>
2024-03-23 15:54:40 -0300

    Fix GIF URL handling

--- src/components/GifCarrusel.jsx
+++ src/components/GifCarrusel.jsx
@@ -1,19 +1,29 @@
+import gif1 from '/1.gif';
+import gif2 from '/2.gif';
+import gif3 from '/3.gif';
+import gif4 from '/4.gif';
+import gif5 from '/5.gif';
+import gif6 from '/6.gif';
+import gif7 from '/7.gif';
+import gif8 from '/8.gif';
+import lose from '/lose.gif';
+import win from '/win.gif';
 
 import PropTypes from 'prop-types';
 
 // Return an object with all the gifs
 const GIFs = () => {
     const gifs = {
-        lose: { url: '/lose.gif', title: 'Lose </3', selected: false },
-        1: { url: '/1.gif', title: 'San Valentin <3', selected: false},
-        2: { url: '/2.gif', title: 'San Valentin <3', selected: false }, 
-        3: { url: '/3.gif', title: 'San Valentin <3', selected: false }, 
-        4: { url: '/4.gif', title: 'San Valentin <3', selected: false }, 
-        5: { url: '/5.gif', title: 'San Valentin <3', selected: false }, 
-        6: { url: '/6.gif', title: 'San Valentin <3', selected: false }, 
-        7: { url: '/7.gif', title: 'San Valentin <3', selected: false }, 
-        8: { url: '/8.gif', title: 'San Valentin <3', selected: false }, 
-        win: { url: '/win.gif', title: 'Win <3', selected: false },
+        lose: { url: lose, title: 'Lose </3', selected: false },
+        1: { url: gif1, title: 'San Valentin <3', selected: false},
+        2: { url: gif2, title: 'San Valentin <3', selected: false }, 
+        3: { url: gif3, title: 'San Valentin <3', selected: false }, 
+        4: { url: gif4, title: 'San Valentin <3', selected: false }, 
+        5: { url: gif5, title: 'San Valentin <3', selected: false }, 
+        6: { url: gif6, title: 'San Valentin <3', selected: false }, 
+        7: { url: gif7, title: 'San Valentin <3', selected: false }, 
+        8: { url: gif8, title: 'San Valentin <3', selected: false }, 
+        win: { url: win, title: 'Win <3', selected: false },
     };
     
     gifs.toArray = () => Object.values(gifs);
