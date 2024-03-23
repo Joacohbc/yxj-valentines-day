c8eac10032d764c80d8041a343afa9e4b3776cd4
Joacohbc <joaquingenovag8@gmail.com>
2024-03-23 02:15:28 -0300

    Change assets to /public

--- src/components/GifCarrusel.jsx
+++ src/components/GifCarrusel.jsx
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 // Return an object with all the gifs
 const GIFs = () => {
     const gifs = {
-        lose: { url: '/src/assets/lose.gif', title: 'Lose </3', selected: false },
-        1: { url: '/src/assets/1.gif', title: 'San Valentin <3', selected: false},
-        2: { url: '/src/assets/2.gif', title: 'San Valentin <3', selected: false }, 
-        3: { url: '/src/assets/3.gif', title: 'San Valentin <3', selected: false }, 
-        4: { url: '/src/assets/4.gif', title: 'San Valentin <3', selected: false }, 
-        5: { url: '/src/assets/5.gif', title: 'San Valentin <3', selected: false }, 
-        6: { url: '/src/assets/6.gif', title: 'San Valentin <3', selected: false }, 
-        7: { url: '/src/assets/7.gif', title: 'San Valentin <3', selected: false }, 
-        8: { url: '/src/assets/8.gif', title: 'San Valentin <3', selected: false }, 
-        win: { url: '/src/assets/win.gif', title: 'Win <3', selected: false },
+        lose: { url: '/lose.gif', title: 'Lose </3', selected: false },
+        1: { url: '/1.gif', title: 'San Valentin <3', selected: false},
+        2: { url: '/2.gif', title: 'San Valentin <3', selected: false }, 
+        3: { url: '/3.gif', title: 'San Valentin <3', selected: false }, 
+        4: { url: '/4.gif', title: 'San Valentin <3', selected: false }, 
+        5: { url: '/5.gif', title: 'San Valentin <3', selected: false }, 
+        6: { url: '/6.gif', title: 'San Valentin <3', selected: false }, 
+        7: { url: '/7.gif', title: 'San Valentin <3', selected: false }, 
+        8: { url: '/8.gif', title: 'San Valentin <3', selected: false }, 
+        win: { url: '/win.gif', title: 'Win <3', selected: false },
     };
     
     gifs.toArray = () => Object.values(gifs);
